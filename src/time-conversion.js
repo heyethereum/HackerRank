62474355496062053e0d9a854213deabcecd097e
heyethereum <heyethereum@gmail.com>
2022-10-16 13:10:01 +0800

    time conversion

--- src/time-conversion.js
+++ src/time-conversion.js
@@ -12,39 +12,18 @@ Time Conversion
 
 function timeConversion(s) {
   // Write your code here
-  /* let militaryHour = "";
-  let amPm = s.charAt(8);
+  let hour = s.slice(0, 2);
 
-  if (amPm == "A") {
-    if (s.substring(0, 2) == "12") {
-      militaryHour = "00";
-    } else {
-      militaryHour = s.substring(0, 2);
-    }
-  } else {
-    // else if amPm == "P"
-    if (s.substring(0, 2) == "12") {
-      militaryHour = s.substring(0, 2);
-    } else {
-      militaryHour = parseInt(s.substring(0, 2), 10) + 12;
-    }
+  if (s.slice(8) === "AM") {
+    return hour === "12" ? "00" + s.slice(2, 8) : s.slice(0, 8);
   }
-  return militaryHour + s.substring(2, 8); */
-  const hour = s.slice(0, 2);
-  if (s.slice(8) === "PM") {
-    if (hour === "12") {
-      return `12${s.slice(2, 8)}`;
-    }
-    return 1 * hour + 12 + s.slice(2, 8);
-  }
-  if (hour === "12") {
-    return `00${s.slice(2, 8)}`;
-  }
-  return s.slice(0, 8);
+  return hour === "12"
+    ? s.slice(0, 8)
+    : parseInt(s.slice(0, 2)) + 12 + s.slice(2, 8);
 }
 
 function main() {
-  const inputString = "12:05:45AM";
+  const inputString = "01:05:45AM";
   console.log("inputString ", inputString);
 
   const result = timeConversion(inputString);
